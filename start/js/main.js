0d522a52801cc54b07ae0cb6dcb6973f59095668
che-d <64429783+che-d@users.noreply.github.com>
2020-12-01 12:17:49 +0300

    w
    w

--- start/js/main.js
+++ start/js/main.js
@@ -43,3 +43,63 @@ $(document).ready(function() {
 		}
 	});
 });
+    /*
+     * animated ellipsis
+     * 
+     * Created by Torsten Walter on 2008-04-06.
+     * Copyright playground.magicrising.de. Some rights reserved.
+     *
+     * This script may be copied and reproduced
+     * if this copyright notice is left intact.
+     */
+    
+     var ellipsis = ['', '.', '..', '...'];
+     var runEllipsis = false;
+     function animateEllipsis(el, count) {
+       el.innerHTML = ellipsis[count%4];
+       if(runEllipsis == true) {
+         window.setTimeout( function(){
+           animateEllipsis(el, ++count);}, 250);
+       }
+     }
+     function startEllipsis() {
+       runEllipsis = true;
+       animateEllipsis(document.getElementById('ellipsisSpan'), 0);
+     }
+     function stopEllipsis() {
+       runEllipsis = false;
+     }
+
+    
+    // just specific example stuff from here
+    
+    // toggles the button between start and stop and runs appropriate function
+    function toggleButton() {
+      var but = document.getElementById('toggleButton');
+      
+      if (but.value == "Start") {
+        startEllipsis();
+        but.value = "Stop";
+      } else {
+        stopEllipsis();
+        but.value = "Start";
+      };
+    }
+    
+    // handle the window.onLoad event
+    function initLoad() {
+      // ad the click event to the toggle button
+      var but = document.getElementById('toggleButton');
+      if (window.attachEvent) {
+        but.attachEvent('onclick', toggleButton);
+      } else if(window.addEventListener) {
+        but.addEventListener('click', toggleButton, true);
+      }
+    }
+    
+    // add the load event this way and it won't interfere with other scripts
+    if (window.attachEvent) {
+      window.attachEvent('onload', initLoad);
+    } else  if(window.addEventListener){
+      window.addEventListener('load', initLoad, true);
+    }
