c7035ca7dd1e1c77d6c6640832e0fb4d0e95c8da
che-d <64429783+che-d@users.noreply.github.com>
2021-06-16 17:15:47 +0300

    animation_b

---
+++ animation/call_wrap/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('.telefone__wrap').hover(function(){
+		$('.telefone__img').addClass('button_run_right').removeClass('button_run_left');;
+		$('.telefone__num').addClass('tel_run_left').removeClass('tel_run_right');
+	}, function() {
+		$('.telefone__img').addClass('button_run_left').removeClass('button_run_right');
+		$('.telefone__num').addClass('tel_run_right').removeClass('tel_run_left');
+	});
+});
