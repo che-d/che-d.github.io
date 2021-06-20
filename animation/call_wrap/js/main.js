27320b94c782d57625bd11456b5d99eab033606d
che-d <64429783+che-d@users.noreply.github.com>
2021-06-20 16:52:42 +0300

    1

--- animation/call_wrap/js/main.js
+++ animation/call_wrap/js/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 	$('.telefone__wrap').hover(function(){
-		$('.telefone__img').addClass('button_run_right').removeClass('button_run_left');;
+		$('.telefone__img_w').addClass('button_run_right').removeClass('button_run_left');;
 		$('.telefone__num').addClass('tel_run_left').removeClass('tel_run_right');
+		// $('.telefone__img_w:after').css({'-webkit-animation': 'pulse1 2s linear infinite','-moz-animation': 'pulse1 2s linear infinite','animation': 'pulse1 2s linear infinite'});
 	}, function() {
-		$('.telefone__img').addClass('button_run_left').removeClass('button_run_right');
+		$('.telefone__img_w').addClass('button_run_left').removeClass('button_run_right');
 		$('.telefone__num').addClass('tel_run_right').removeClass('tel_run_left');
+		// $('.telefone__img_w:after').css('button_run_left');
 	});
 });
