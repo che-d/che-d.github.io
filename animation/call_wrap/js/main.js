27c6588c2952d53542cf4a28145fa50c496314d1
che-d <64429783+che-d@users.noreply.github.com>
2021-06-20 18:08:34 +0300

    1

--- animation/call_wrap/js/main.js
+++ animation/call_wrap/js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	$('.telefone__wrap').hover(function(){
-		$('.telefone__img_w').addClass('button_run_right').removeClass('button_run_left');;
+		$('.telefone__img_wrap').addClass('button_run_right').removeClass('button_run_left');;
 		$('.telefone__num').addClass('tel_run_left').removeClass('tel_run_right');
-		// $('.telefone__img_w:after').css({'-webkit-animation': 'pulse1 2s linear infinite','-moz-animation': 'pulse1 2s linear infinite','animation': 'pulse1 2s linear infinite'});
+		// $('.telefone__img_wrap:after').css({'-webkit-animation': 'pulse1 2s linear infinite','-moz-animation': 'pulse1 2s linear infinite','animation': 'pulse1 2s linear infinite'});
 	}, function() {
-		$('.telefone__img_w').addClass('button_run_left').removeClass('button_run_right');
+		$('.telefone__img_wrap').addClass('button_run_left').removeClass('button_run_right');
 		$('.telefone__num').addClass('tel_run_right').removeClass('tel_run_left');
-		// $('.telefone__img_w:after').css('button_run_left');
+		// $('.telefone__img_wrap:after').css('button_run_left');
 	});
 });
