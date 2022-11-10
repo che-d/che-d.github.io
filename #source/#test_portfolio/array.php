b0d7507c4aa0d281b161a86dda7fce206f8d2075
che-d <64429783+che-d@users.noreply.github.com>
2022-11-10 17:06:26 +0300

    12

---
+++ #source/#test_portfolio/array.php
@@ -0,0 +1,23 @@
+<?
+$arr_sites = array(
+	array(
+		'ID' => 'tim',
+		'NAME' => 'Тепло Изоляционные Материалы',
+		'URL' => 'https://tim-firm.ru/',
+		'DOMEN' => 'tim-firm.ru',
+		'LINK_IMG' => 'img/1566x696/tim.jpg',
+		'WORK' => 'Верстка',
+		'DESCRIPTION' => 'Прождажа теплоизоляционных материалов',
+		'CLASS' => '',
+	),
+	array(
+		'ID' => '',
+		'NAME' => '',
+		'URL' => '',
+		'DOMEN' => '',
+		'LINK_IMG' => '',
+		'WORK' => '',
+		'DESCRIPTION' => '',
+		'CLASS' => '',
+	),
+?>
